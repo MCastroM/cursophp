bf58d66277f794397735148250c0fe74fbbd940e
Marco Castro <user.mail>
2023-10-19 08:24:21 -0300

    mvc

--- carrito/controllers/UsuarioController.php
+++ carrito/controllers/UsuarioController.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'models/usuario.php';
 class usuarioController{
 
     public function index(){
@@ -10,10 +10,48 @@ class usuarioController{
         require_once 'views/usuario/registro.php';
     }
 
-    public function save(){
-        if(isset($_POST)){
-            echo '<pre>' , var_dump($_POST) , '</pre>';
-        }
-       
-    }
+	public function save(){
+		if(isset($_POST)){
+			
+			$nombre = isset($_POST['nombre']) ? $_POST['nombre'] : false;
+			$apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
+			$email = isset($_POST['email']) ? $_POST['email'] : false;
+			$password = isset($_POST['password']) ? $_POST['password'] : false;
+			
+			if($nombre && $apellidos && $email && $password){
+				$usuario = new Usuario();
+				$usuario->setNombre($nombre);
+				$usuario->setApellidos($apellidos);
+				$usuario->setEmail($email);
+				$usuario->setPassword($password);
+
+				$save = $usuario->save();
+				if($save){
+					$_SESSION['register'] = "complete";
+				}else{
+					$_SESSION['register'] = "failed";
+				}
+			}else{
+				$_SESSION['register'] = "failed";
+			}
+		}else{
+			$_SESSION['register'] = "failed";
+		}
+		header("Location:".base_url.'usuario/registro');
+	}
+
+	public function  login(){
+		if(isset($_POST)){
+			// Identificar Usuario
+
+			// Consulta  ala base de datos
+
+			$usuario = new Usuario();
+			$usuario->setEmail($_POST['email']);
+			$usuario->setPassword($_POST['password']);
+
+			$identity = $usuario->login();
+		}
+		header("Location:".base_url);
+	}
 }
