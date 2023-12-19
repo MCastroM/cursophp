2293c1ab8ab624ad9d40b0ee8e00915ee6cfc512
Marco Castro <user.mail>
2023-12-19 17:05:20 -0300

    laravel

---
+++ proyecto-laravel/app/Models/Image.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model
+{
+    use HasFactory;
+
+    protected $table = 'images';
+
+    // Relación One to Many
+    public function comments(){
+        return $this->hasMany('App\Models\Comment');
+
+    }
+
+    // Relación One to Many
+    public function likes(){
+        return $this->hasMany('App\Models\Like');
+
+    }
+
+     // Relación Many to One
+    public function user(){
+        return $this->belongsTo('App\Models\User', 'user_id');
+
+    }
+
+}
---
+++ proyecto-laravel/app/Models/Like.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    use HasFactory;
+
+    protected $table = 'likes';
+
+    // Relación Many to One
+    public function user(){
+        return $this->belongsTo('App/User', 'user_id');
+
+    }
+
+    // Relación Many to One
+    public function image(){
+        return $this->belongsTo('App/Image', 'image_id');
+
+    }
+
+}
